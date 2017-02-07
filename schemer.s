81c9011b41de8a4fc382eb0b2b04ce3bf1fbbc52
Kevin Kays <kkays@vt.edu>
2017-02-07 12:54:22 -0500

    multiinsertR

--- schemer.s
+++ schemer.s
@@ -68,3 +68,14 @@
       ((null? lat) '())
       ((eq? (car lat) a) (multirember a (cdr lat)))
       (else (cons (car lat) (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+          (else (cons (car lat)
+                  (multiinsertR new old (cdr lat)))))))))
+
