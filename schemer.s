a9b8805dd276ee3d5b9e8972c8de25ca569f3274
Kevin Kays <kkays@vt.edu>
2017-02-07 12:31:00 -0500

    subst

--- schemer.s
+++ schemer.s
@@ -35,3 +35,20 @@
           ((eq? (car lat) old) (cons old (cons new (cdr lat))))
           (else (cons (car lat)
                   (insertR new old (cdr lat)))))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? (car lat) old) (cons new (cons old (cdr lat))))
+          (else (cons (car lat)
+                  (insertL new old (cdr lat)))))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (cdr lat)))
+      (else (cons (car lat) (subst new old (cdr lat)))))))
