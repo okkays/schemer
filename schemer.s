a4a05586d4ea0c02603e8224812735b00ddc3097
Kevin Kays <kkays@vt.edu>
2017-02-07 11:23:33 -0500

    rember v1

--- schemer.s
+++ schemer.s
@@ -11,3 +11,13 @@
       ((null? lat) #f)
       (else (or (eq? (car lat) a)
               (member? a (cdr lat)))))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((eq? (car lat) a) (cdr lat))
+              (else (cons (car lat)
+                          (rember a (cdr lat)))))))))
+
