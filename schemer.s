66cff2cceb423c0a2ad621ac8b2627020936ceaa
Kevin Kays <kkays@vt.edu>
2017-02-07 12:11:05 -0500

    insertR

--- schemer.s
+++ schemer.s
@@ -25,3 +25,13 @@
       ((null? l) '())
       (else (cons (car (car l))
         (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+          (else (cons (car lat)
+                  (insertR new old (cdr lat)))))))))
