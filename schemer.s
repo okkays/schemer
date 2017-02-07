57c1c39e5726bb7d5a7c18137b6f99f185b3c954
Kevin Kays <kkays@vt.edu>
2017-02-07 11:43:09 -0500

    firsts

--- schemer.s
+++ schemer.s
@@ -18,3 +18,10 @@
       ((null? lat) '())
       ((eq? (car lat) a) (cdr lat))
       (else (cons (car lat) (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l))
+        (firsts (cdr l)))))))
