597fb426d4633a6258a7ba473396a08dd71ea51c
Kevin Kays <kkays@vt.edu>
2017-02-07 10:55:31 -0500

    add member?

--- schemer.s
+++ schemer.s
@@ -4,3 +4,10 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+              (member? a (cdr lat)))))))
